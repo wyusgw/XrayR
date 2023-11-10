9dac8d72b16c94f9c888a88345015f1230bdb82f
StarCloud <115127996+StarNGK@users.noreply.github.com>
2023-11-10 20:44:19 +0800

    Update gov2panel_test.go

--- api/gov2panel/gov2panel_test.go
+++ api/gov2panel/gov2panel_test.go
@@ -3,8 +3,8 @@ package gov2panel_test
 import (
 	"testing"
 
-	"github.com/wyx2685/XrayR/api"
-	"github.com/wyx2685/XrayR/api/gov2panel"
+	"github.com/StarNGK/XrayR/api"
+	"github.com/StarNGK/XrayR/api/gov2panel"
 )
 
 func CreateClient() api.API {
