34fbef0577ff3fb1f4e4fc0a1c2df67ba3e9323a
StarCloud <115127996+StarNGK@users.noreply.github.com>
2023-11-10 20:47:41 +0800

    Update v2raysocks_test.go

--- api/v2raysocks/v2raysocks_test.go
+++ api/v2raysocks/v2raysocks_test.go
@@ -3,8 +3,8 @@ package v2raysocks_test
 import (
 	"testing"
 
-	"github.com/wyx2685/XrayR/api"
-	"github.com/wyx2685/XrayR/api/v2raysocks"
+	"github.com/StarNGK/XrayR/api"
+	"github.com/StarNGK/XrayR/api/v2raysocks"
 )
 
 func CreateClient() api.API {
